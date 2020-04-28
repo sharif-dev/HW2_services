78f76f54be06f2f0a80b80245ce62b42ded78c1f
hhosseini1377 <hhosseini@ce.sharif.edu>
2020-04-28 16:26:20 +0430

    service one added

---
+++ app/src/main/java/AlarmReciever.java
@@ -0,0 +1,21 @@
+
+package com.example.sensors;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        Toast.makeText(context, "Alarm received!", Toast.LENGTH_LONG).show();
+
+
+
+        Intent i = new Intent(context, AlarmActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+
+
+    }
+}
